5ef456ac9ede0f32bde848a2ca13f49a0ac34eae
Scott Young <syoung@instructure.com>
2020-10-20 11:54:52 -0600

    initial commit

---
+++ src/blog/blog.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post } from './interfaces/post.interface';
+import { CreatePostDTO } from './dto/create-post.dto';
+
+
+@Injectable()
+export class BlogService {
+    constructor(@InjectModel('Post') private readonly postModel: Model<Post>) {  }
+
+    async addPost(createPostDTO: CreatePostDTO): Promise<Post> {
+        const newPost = await this.postModel(CreatePostDTO);
+        return newPost.save();
+    }
+
+    async getPost(PostID): Promise<Post> {
+        const post = await this.postModel
+        .findById(PostID)
+        .exec();
+        return post;
+    }
+
+    async getPosts(): Promise<Post[]> {
+        const posts = await this.postModel.find().exec();
+        return posts;
+    }
+
+    async editPost(PostID, createPostDTO: CreatePostDTO): Promise<Post> {
+        const editedPost = await this.postModel
+        .findByIdAndUpdate(PostID, createPostDTO, { new: true });
+        return editedPost;
+    }
+
+    async deletePost(PostID): Promise<any>{
+        const deletedPost = await this.postModel
+        .findByIdAndRemove(PostID);
+        return deletedPost;
+    }
+}

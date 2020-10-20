d29af9794498a9449ff01a9f664644273c3b3e35
Scott Young <syoung@instructure.com>
2020-10-20 15:32:07 -0600

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   Sentry.init({
     dsn: "https://51206585622d436585b5706930964b3d@o451084.ingest.sentry.io/5473914",
   });
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
